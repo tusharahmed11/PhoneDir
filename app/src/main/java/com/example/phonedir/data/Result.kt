eaff15c362ca14c866f956f4a40e0959d1bd84db
Tushar ahmed <ahmedorno11@gmail.com>
2024-05-16 19:02:44 +0600

    hilt compiler

--- app/src/main/java/com/example/phonedir/data/Result.kt
+++ app/src/main/java/com/example/phonedir/data/Result.kt
@@ -8,6 +8,7 @@ sealed class Result<out T : Any> {
 
     data class Success<out T : Any>(val data: T) : Result<T>()
     data class Error(val exception: Exception) : Result<Nothing>()
+    data class Loading() : Result<Nothing>()
 
     override fun toString(): String {
         return when (this) {
